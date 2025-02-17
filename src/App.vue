9a5cd7f62e84f6fe7ff086473cb867dd01ee85e1
Oleksii_Pivniev <102686871+Oleksii03@users.noreply.github.com>
2025-02-17 18:15:55 +0200

    фвв

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
 <template>
   <div>
     <h1>гадаєш вишлдо</h1>
+    <p>важко сказати</p>
   </div>
 </template>
 
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,52 @@
+// import { defineConfig } from "vite";
+// import vue from "@vitejs/plugin-vue";
+// import path from "path";
+
+// export default defineConfig({
+//   base: "/Taste",
+//   plugins: [vue()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// });
+
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   base: "/Taste",
   plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "./src/index.html"),
+        about: path.resolve(__dirname, "./src/about.html"),
+        contact: path.resolve(__dirname, "./src/contact.html"),
+        // Додайте більше сторінок, як потрібно
+      },
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+        entryFileNames: ({ name }) => {
+          return name === "index"
+            ? "js/[name].[hash].js"
+            : "js/[name]/[name].[hash].js";
+        },
+      },
+    },
+    outDir: "../dist",
+    chunkSizeWarningLimit: 500,
+  },
 });

bb02ac77af86da985dec55a1ee026373b5c27d1f
Oleksii_Pivniev <102686871+Oleksii03@users.noreply.github.com>
2025-02-24 12:30:52 +0200

    oleksii-dev: 'Ignore .vscode folder'

--- vite.config.js
+++ vite.config.js
@@ -17,6 +17,8 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@js": path.resolve(__dirname, "src/js"),
     },
   },
   build: {

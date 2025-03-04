179f63c5142b0faeef495e530579164409f7482a
Oleksii_Pivniev <102686871+Oleksii03@users.noreply.github.com>
2025-03-04 12:31:34 +0200

    fix

--- src/components/UI/UISmallButton.vue
+++ src/components/UI/UISmallButton.vue
@@ -1,17 +1,3 @@
-<template>
-  <button class="small-button">
-    <slot></slot>
-  </button>
-</template>
+<template></template>
 
-<script setup></script>
-
-<style lang="scss" scoped>
-.small-button {
-  border: 1px solid rgba(255, 255, 255, 0.1);
-  border-radius: 12px;
-  width: 40px;
-  height: 40px;
-  background: transparent;
-}
-</style>
+<style lang="scss" scoped></style>
